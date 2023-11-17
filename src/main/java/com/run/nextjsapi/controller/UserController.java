938281d756b37e8940c39d6109d81e254181d75f
AarynLu <1298911600@qq.com>
2023-11-17 19:08:39 +0800

    feat: data filter, search and order

--- src/main/java/com/run/nextjsapi/controller/UserController.java
+++ src/main/java/com/run/nextjsapi/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.run.nextjsapi.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.run.nextjsapi.common.Result;
 import com.run.nextjsapi.common.ResultCode;
+import com.run.nextjsapi.entity.Team;
 import com.run.nextjsapi.entity.User;
+import com.run.nextjsapi.entity.params.UserFilterParams;
+import com.run.nextjsapi.service.TeamService;
 import com.run.nextjsapi.service.UserService;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
@@ -14,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * @desc:
+ * @desc: TODO: 查询出 Team
  * @author: AruNi_Lu
  * @date: 2023/11/16
  */
@@ -27,24 +32,67 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    @Resource
+    private TeamService teamService;
+
     @RequestMapping(path = "/createOrUpdate", method = RequestMethod.POST)
-    public Result createOrUpdate(@RequestBody User user) {
-        boolean isSave = userService.saveOrUpdate(user);
-        if (isSave) {
-            return Result.ok();
-        } else {
-            logger.error("createOrUpdate error");
-            return Result.build(ResultCode.DATABASE_ERROR);
-        }
+    public Boolean createOrUpdate(@RequestBody User user) {
+        return userService.saveOrUpdate(user);
     }
 
     @RequestMapping(path = "/getUserList", method = RequestMethod.GET)
-    public Result getUserList(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                              @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+    public Result getUserList(@RequestParam("jsonParams") String jsonString) {
+        /*
+          jsonParams: {
+            "pagination": {
+              "current": 1,
+              "pageSize": 10
+            },
+            "filters": {
+              "age": [
+                "20"
+              ],
+              "gender": [
+                "male"
+              ]
+            },
+            "order": "ascend",
+            "field": "name",
+            “searchUsername": "AruNi_Lu"
+          }
+         */
+        logger.info("getUserList jsonParams: {}", jsonString);
         QueryWrapper<User> wrapper = new QueryWrapper<>();
+
+        UserFilterParams userFilterParams = JSON.parseObject(jsonString, UserFilterParams.class);
+        int current = userFilterParams.getPagination().getCurrent();
+        int pageSize = userFilterParams.getPagination().getPageSize();
+        if (userFilterParams.getField() != null && userFilterParams.getOrder() != null) {
+            wrapper.orderBy(true, userFilterParams.getOrder().equals("ascend"), userFilterParams.getField());
+        }
         wrapper.orderByDesc("create_time");
 
-        Page<User> userPage = new Page<>(page, pageSize, true);
+        if (userFilterParams.getFilters() != null) {
+            if (userFilterParams.getFilters().getGender() != null) {
+                wrapper.in("gender", userFilterParams.getFilters().getGender());
+            }
+            List<String> teams = userFilterParams.getFilters().getTeam();
+            if (teams != null) {
+                List<Integer> teamIds = teamService.list(new QueryWrapper<Team>()
+                                .in("team_name", teams))
+                        .stream()
+                        .map(Team::getId)
+                        .toList();
+                wrapper.in(!teamIds.isEmpty(), "team_id", teamIds);
+            }
+        }
+
+        // 搜索参数
+        if (userFilterParams.getSearchUsername() != null) {
+            wrapper.like("name", userFilterParams.getSearchUsername());
+        }
+
+        Page<User> userPage = new Page<>(current, pageSize, true);
         try {
             Page<User> pageResult = userService.page(userPage, wrapper);
             List<User> userList = pageResult.getRecords();
---
+++ src/main/java/com/run/nextjsapi/entity/params/UserFilterParams.java
@@ -0,0 +1,35 @@
+package com.run.nextjsapi.entity.params;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @desc:
+ * @author: AruNi_Lu
+ * @date: 2023/11/17
+ */
+@Data
+public class UserFilterParams {
+    private Pagination pagination;
+    private Filters filters;
+    private String field;
+    private String order;
+    private String searchUsername;
+
+    @Data
+    public static class Pagination {
+        private int current;
+        private int pageSize;
+    }
+
+    @Data
+    public static class Filters {
+        private List<String> name;
+        private List<Integer> age;
+        private List<String> gender;
+        private List<String> phone;
+        private List<String> team;
+    }
+}
